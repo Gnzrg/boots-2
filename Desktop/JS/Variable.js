a5cdc011fe1e1230485a52b8499a7730d267f948
Morning <morning@admins-iMac.local>
2022-11-28 12:10:53 +0800

    day-1

---
+++ Desktop/JS/Variable.js
@@ -0,0 +1,73 @@
+//1-9
+
+//Comments can make code readeble
+        //Javascript lesson begin
+        /*Comments can make code readeble*/
+        
+        let message = 'hello';
+        console.log(message)
+        console.log(typeof(message));
+        let test = 'hello2';
+        console.log(test)
+        console.log(typeof(test));
+        var a =100;
+        console.log(a);
+        console.log(typeof(a));
+        var b=null;
+        console.log(b);
+        console.log(typeof(b));
+var c=true;
+console.log(c) 
+console.log(typeof(c))
+var d;
+console.log(d)
+
+
+var firstName = "Ganzorig"
+console.log(firstName);
+var lastName = 'Bat-Erdene'
+console.log(lastName);
+var maritalStatus = 'hello'
+console.log(maritalStatus);
+var country = 'Mongolia'
+console.log(country)
+var age = 18
+console.log(age);
+var firstName ="Ganzorig" , lastName = 'Bat-Erdene', maritalStatus = 'hello'
+
+
+//21
+let x= 1;
+let y= 2;
+let sum=x+y;
+let sub=x-y;
+let mul=x*y;
+let div=x/y;
+let mod=x%y
+
+console.log(sum, sub, mul, div, mod)
+//16
+var gb=15;
+var data=gb*8*(10**10)
+console.log(data)
+//17
+let mb=15;
+let sMB= mb/3;
+let bitRate=sMB*8*(10**6);
+console.log(bitRate)
+//12
+var q=2;
+var r=5;
+var j=3.14;
+var area= j*r**q;
+console.log(area)
+//11
+var side1= 10;
+var side2=15;
+var side3=20;
+var p=side1+side2+side3;
+console.log(p)
+//19
+let t=100;
+let f=100+32
+console.log(f)
